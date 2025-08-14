880236304f3e118038fa610e64639eef0386f17f
Cosmic0921 <85378869+Cosmic0921@users.noreply.github.com>
2025-08-13 22:10:26 -0500

    Square Bouncing Example

--- game-src/main.c
+++ game-src/main.c
@@ -7,37 +7,52 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    SDL_Window* win = SDL_CreateWindow("Hello SDL2", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (win == NULL) {
+    SDL_Window* win = SDL_CreateWindow("Bouncing Square", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (!win) {
         printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (ren == NULL) {
+    if (!ren) {
         SDL_DestroyWindow(win);
         printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
+    SDL_Rect square = { 100, 100, 50, 50 };
+    int speedX = 3;
+    int speedY = 3;
+
     int running = 1;
     SDL_Event e;
+
     while (running) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
                 running = 0;
             }
         }
+        square.x += speedX;
+        square.y += speedY;
 
-        SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
+        if (square.x < 0 || square.x + square.w > 640) speedX = -speedX;
+        if (square.y < 0 || square.y + square.h > 480) speedY = -speedY;
+
+        SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
         SDL_RenderClear(ren);
+        
+        SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
+        SDL_RenderFillRect(ren, &square);
+
         SDL_RenderPresent(ren);
     }
 
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
+
     return 0;
-} 
+}
